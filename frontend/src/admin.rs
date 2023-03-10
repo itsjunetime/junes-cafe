37f106f745aed63d52cdfff0dc6541e9c5c068cd
itsjunetime <junewelker@gmail.com>
2023-03-09 23:14:01 -0700

    Actually make the admin page pretty

--- frontend/src/admin.rs
+++ frontend/src/admin.rs
@@ -11,22 +11,33 @@ impl PostViewProvider for AdminPostView {
 	fn post_view(post: &Post) -> Html {
 		html! {
 			<div class="post" id={ format!("post-{}", post.id) }>
-				<h2 class="post-title">{ &post.title }</h2>
-				{
-					post.tags.0.iter().map(|tag|
-						html! { <span class="tag">{ tag }</span> }
-					).collect::<Html>()
-				}
-				<div class="post-subtitle-box">
-					<span class="post-subtitle">
-						{ "by " }
-						<strong>{ post.display_user() }</strong>
-						{ ", " }
-						<strong>{
-							NaiveDateTime::from_timestamp_opt(post.created_at as i64, 0)
-								.map(|dt| dt.format("%H:%M on %b %-d, %Y").to_string())
-								.unwrap_or_else(|| "an unknown time".into())
-						}</strong>
+				<div class="post-top">
+					<h2 class="post-title">{ &post.title }</h2>
+					<div class="post-subtitle-box">
+						<span class="post-subtitle">
+							{ "by " }
+							<strong>{ post.display_user() }</strong>
+							{ ", " }
+							<strong>{
+								NaiveDateTime::from_timestamp_opt(post.created_at as i64, 0)
+									.map(|dt| dt.format("%H:%M on %b %-d, %Y").to_string())
+									.unwrap_or_else(|| "an unknown time".into())
+							}</strong>
+						</span>
+					</div>
+				</div>
+				<hr/>
+				<div class="post-bottom">
+					<span class="tags">
+						{
+							post.tags.0.iter().map(|tag|
+								html! { <span class="tag">{ tag }</span> }
+							).collect::<Html>()
+						}
+					</span>
+					<span class="action-links">
+						<a class="action-link" href={ format!("/post/{}", post.id) }>{ "View" }</a>
+						<a class="action-link" href={ format!("/edit_post/{}", post.id) }>{ "Edit" }</a>
 					</span>
 				</div>
 			</div>
@@ -36,7 +47,54 @@ impl PostViewProvider for AdminPostView {
 
 #[function_component(Admin)]
 pub fn admin(props: &HomeProps) -> Html {
-	html! {
+	html! { <>
+		<style>
+		{ "
+		.post {
+			display: flex;
+			flex-flow: wrap;
+		}
+		.post hr {
+			flex-basis: 100%;
+			margin: 0;
+			border: 0;
+		}
+		.post-title {
+			margin: 0px;
+			align-items: flex-end;
+		}
+		.post-bottom, .post-top {
+			display: flex;
+			justify-content: space-between;
+			flex-grow: 100;
+		}
+		.post-bottom {
+			margin: 8px 0px 4px 0px;
+		}
+		.tags, .action-links {
+			display: flex;
+			gap: 8px;
+		}
+		.tags {
+			justify-content: flex-start;
+		}
+		.tag {
+			margin-right: 0px !important;
+		}
+		.action-links {
+			justify-content: flex-end;
+		}
+		a.action-link {
+			text-decoration: none;
+			padding: 4px 8px;
+			border-radius: 6px;
+			border: 2px solid var(--secondary-background);
+		}
+		a.action-link:hover {
+			background-color: var(--secondary-background);
+		}
+		" }
+		</style>
 		<PostList<AdminPostView> page={ props.page } title={ "Admin" } post_view={ PhantomData }/>
-	}
+	</>}
 }
